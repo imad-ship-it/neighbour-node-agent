01eacd2ef4c2ff7f47d0ee2a615c21fa7018d915
imad-ship-it <imadworks9@gmail.com>
2026-07-21 23:58:33 +0500

    add Bookmark model

--- backend/apps/bookmarks/models.py
+++ backend/apps/bookmarks/models.py
@@ -1,1 +1,20 @@
 # Create your models here.
+from apps.listings.models import Listing
+from django.conf import settings
+from django.db import models
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="bookmarks"
+    )
+    listing = models.ForeignKey(
+        Listing, on_delete=models.CASCADE, related_name="bookmarks"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "listing")
+
+    def __str__(self):
+        return f"{self.user} → {self.listing}"

13e1977336b8d884e52f9c52d063ce9b6b6164d9
imad-ship-it <imadworks9@gmail.com>
2026-07-23 23:22:35 +0500

    add axios client with token interceptor

---
+++ frontend/src/api/client.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { getAccessToken } from './tokenStore'
+
+const client = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api',
+})
+
+client.interceptors.request.use((config) => {
+  const token = getAccessToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default client
